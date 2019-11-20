9ef4454be8667491e86288dc6a06524f62fff32a
Joshoa Carrasco <joshmi1902@gmail.com>
2019-11-20 08:37:08 -0500

    semi

--- 3.py
+++ 3.py
@@ -1,10 +1,12 @@
 import os 
+import random
 os.system("cls")
 
 m = int(input("ingrese orden: "))
 if (m > 2 and m < 5):
     numx = random.randint(1,100)
-for i in m1:
-    m1 = []
-    m1.append(numx)
-    print(m1)
+for i in x:
+    x = [[],[]]
+    x.append(numx)
+
+print(x)
