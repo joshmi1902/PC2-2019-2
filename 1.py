b3a15d36fb22c0f84856b73e44579f519fa52b68
Joshoa Carrasco <joshmi1902@gmail.com>
2019-11-20 08:38:25 -0500

    semi

--- 1.py
+++ 1.py
@@ -2,5 +2,13 @@ import os
 os.system("cls")
 
 def contarletra (texto,letra):
+    #letra dentro del txt
     for letra in texto:
-        print(len(letra)
+        if letra.count(texto) == 0:
+            print(letra)
+    return contarletra
+
+#ejemplos:
+
+x = contarletra("dificilisimo", "i")
+print(x)
