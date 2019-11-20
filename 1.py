10eb85a7b729a49e6f829691a55e53482b192981
Joshoa Carrasco <joshmi1902@gmail.com>
2019-11-20 08:22:22 -0500

    a medias

--- 1.py
+++ 1.py
@@ -1,2 +1,7 @@
 import os
 os.system("cls")
+
+def contarletra (texto,letra):
+    for letra in texto:
+        print(len(letra)
+
---
+++ 3.py
@@ -0,0 +1,10 @@
+import os 
+os.system("cls")
+
+m = int(input("ingrese orden: "))
+if (m > 2 and m < 5):
+    numx = random.randint(1,100)
+for i in m1:
+    m1 = []
+    m1.append(numx)
+    print(m1)
